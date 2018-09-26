8a856afa8edff8fe5911c42855075fd5acf3ffeb
kamojiro <tamagoma002@yahoo.co.jp>
2018-09-26 21:34:14 +0900

    いくつかやってみた。g++を忘れていた。

---
+++ nyumon/hello2.cpp
@@ -0,0 +1,12 @@
+#include<string>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  string name;
+  cout << "こんにちわ、私は加藤です。" << endl;
+  cout << "君の名は" << endl;
+  cin >> name;
+  cout << name << "さん。よろしく。" << endl;
+}
